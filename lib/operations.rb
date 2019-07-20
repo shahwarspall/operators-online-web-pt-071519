0621cc0fbd98fda58b762f94bb1e21c127882289
Shahwar Spall <shevy.spall@gmail.com>
2019-07-20 17:00:30 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
   if speed >= 60 
     return TRUE
-    elsif speed <= 40
+  elsif speed <= 40
     return TRUE
-  elsif speed 40 < 60
+  elsif speed 40 > 60
     return FALSE
 
 end
@@ -12,11 +12,7 @@ end
 
 
 def not_safe?(speed)
-	if speed >=60 
-	  return TRUE
-	elsif speed <= 40 
-	  return TRUE
-	elsif speed 
+
 end
 	
 
